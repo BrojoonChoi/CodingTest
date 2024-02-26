9ae104bdd8a32b8f8d236b575c332e415980c61c
wanjoon <wanjnovember@naver.com>
2024-02-26 14:13:19 +0100

    read me 수정

--- src/components/topicItem.tsx
+++ src/components/topicItem.tsx
@@ -7,7 +7,7 @@ type ITopicProps = {
 
 const TopicItem:React.FC<ITopicProps> = ({infomation}) => {
   return (
-    <div style={{display:'flex', flexDirection:'column', width:'200px', borderRadius:'2px', borderColor:'gray', borderWidth:'6px', alignItems:'center', borderStyle:'solid', marginLeft:'1rem', marginRight:'1rem'}}>
+    <div style={{display:'flex', flexDirection:'column', width:'200px', borderRadius:'16px', borderColor:'gray', borderWidth:'6px', alignItems:'center', borderStyle:'solid', marginLeft:'1rem', marginRight:'1rem'}}>
       <span style={{textWrap:'wrap'}}>{infomation.title}</span>
       <img src={infomation.imgPath} style={{ width: '150px', height: '150px', objectFit: 'scale-down' }}/>
     </div>
